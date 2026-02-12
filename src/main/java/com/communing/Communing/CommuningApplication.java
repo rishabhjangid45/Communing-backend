69a3c23f0efd83d7870fd98462b8c665d98240f1
rishabhjangid45 <rishabhh405@gmail.com>
2026-02-12 21:15:52 +0530

    Initialising project boilerplate code.

---
+++ src/main/java/com/communing/Communing/CommuningApplication.java
@@ -0,0 +1,13 @@
+package com.communing.Communing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommuningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommuningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/communing/Communing/CommuningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.communing.Communing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommuningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
