978bbe4964a3884b0f2abe4f143fd7efe8c82ba5
pratikshabansal <pratikshaagarwal.cse27@jecrc.ac.in>
2026-02-14 20:54:34 +0530

    test

--- src/main/java/com/communing/Communing/CommuningApplication.java
+++ src/main/java/com/communing/Communing/CommuningApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CommuningApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(CommuningApplication.class, args);
+		System.out.println("pratiksha");
 	}
 
 }
